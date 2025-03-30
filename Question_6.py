34cfe1562298e4c4974033a33965622bcc8585bd
Klein-Baru <130561023+Klein-Baru@users.noreply.github.com>
2025-03-30 17:27:48 +0300

    added files in case .ipynb doesnt load

--- Question_6.py
+++ Question_6.py
@@ -1,33 +1,46 @@
+# Question 6: Count Vowels
+# Write a program that counts the number of vowels in a sentence
+
 sentence = input("Enter an English word/sentence:")
 count = 0
+dont_repeat_a = 0
+dont_repeat_e = 0
+dont_repeat_i = 0
+dont_repeat_o = 0
+dont_repeat_u = 0
 
 characterize = [i for i in sentence]
 
 for character in characterize:
 
-    if character == "a" or character == "A":
+    if (character == "a" or character == "A") and dont_repeat_a == 0:
 
         count += 1
+        dont_repeat_a = 1
         continue
 
-    elif character == "e" or character == "E":
+    elif (character == "e" or character == "E") and dont_repeat_e == 0:
 
         count += 1
+        dont_repeat_e = 1
         continue
 
-    elif character == "i" or character == "I":
+    elif (character == "i" or character == "I") and dont_repeat_i == 0:
 
         count += 1
+        dont_repeat_i = 1
         continue
 
-    elif character == "o" or character == "O":
+    elif (character == "o" or character == "O") and dont_repeat_o == 0:
 
         count += 1
+        dont_repeat_o = 1
         continue
 
-    elif character == "u" or character == "U" :
+    elif (character == "u" or character == "U") and dont_repeat_u == 0:
 
         count += 1
+        dont_repeat_u = 1
         continue
 
     else:
@@ -35,6 +48,12 @@ for character in characterize:
         pass
 
 print(count)
+
+
+
+
+
+
 
 
 
