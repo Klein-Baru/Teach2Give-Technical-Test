f34663198fe50112f6e36da6ee570705ef3f351d
Klein-Baru <130561023+Klein-Baru@users.noreply.github.com>
2025-03-30 17:35:21 +0300

    added file in case .ipynb does not load

--- Question_1.py
+++ Question_1.py
@@ -1,17 +1,19 @@
-for i in range(1, 101):
+# Question 1: FizzBuzz
+# Write a program that prints the numbers from 1 to 100. For multiples of 3, print "Fizz"; for
+# multiples of 5, print "Buzz"; and for numbers that are multiples of both 3 and 5, print "FizzBuzz".for i in range(1, 101):
 
-    if i % 3 == 0 and i % 5 == 0:
-        print("FizzBuzz")
-        continue
+if i % 3 == 0 and i % 5 == 0:
+    print("FizzBuzz")
+    continue
 
-    elif i % 3 == 0:
-        print("Fizz")
-        continue
+elif i % 3 == 0:
+    print("Fizz")
+    continue
 
-    elif i % 5 == 0:
-        print("Buzz")
-        continue
+elif i % 5 == 0:
+    print("Buzz")
+    continue
 
-    else:
-        print(i)
-        continue
+else:
+    print(i)
+    continue
