9ee4c3e3875ae24e58dd92e1c8c7c902174faefb
Klein-Baru <130561023+Klein-Baru@users.noreply.github.com>
2025-03-30 17:32:44 +0300

    added file in case the .ipynb does not load

--- Question_3.py
+++ Question_3.py
@@ -1,3 +1,6 @@
+#  Question 3: Power of Two
+#  Write a program that takes an integer as input and returns true if the input is a power of two.
+
 import math
 
 user_input = int(input("Enter any number: "))
